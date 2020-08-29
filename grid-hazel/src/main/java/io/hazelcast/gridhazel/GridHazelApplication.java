f9f40d8bb0d9972461347fd6bf82382d040af5d8
abhilashgundlapally <abhilashgundlapally@gmail.com>
2020-08-29 23:28:31 +0530

    Add files via upload
    sample spring boot project (java 11)

---
+++ grid-hazel/src/main/java/io/hazelcast/gridhazel/GridHazelApplication.java
@@ -0,0 +1,13 @@
+package io.hazelcast.gridhazel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridHazelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridHazelApplication.class, args);
+	}
+
+}
